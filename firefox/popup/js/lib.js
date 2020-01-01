409ca4d1c6d02bce16ae129ccda1eadad29f52c4
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2020-01-01 03:38:17 -0300

    Função countingChars() experimental. Estilizar disabled. Limpar quando apagar.

--- firefox/popup/js/lib.js
+++ firefox/popup/js/lib.js
@@ -58,3 +58,31 @@ export function isEmpty(txt) {
         return true;
     }
 }
+
+
+var input = document.querySelector('#text-to-be-transformed');
+
+input.addEventListener("keydown", countingChars);
+
+var counter = document.querySelector('#counter');
+var result = document.createElement('span');
+var btn = document.querySelector('#transform-text');
+function countingChars() {
+    let max = 10;
+    let count = 0;
+    let textLength = input.value.length;
+    count = max - textLength;
+    if(count <= 0){
+        result.style.color = "red";
+        btn.disabled = true;
+    } else{
+        result.style.color = "#404040";
+        btn.disabled = false;
+    }
+    
+    result.textContent = count + "/10";
+    
+    counter.appendChild(result);
+
+    
+}
